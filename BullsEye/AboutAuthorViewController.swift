39f8b852ca9ab17b40482e1ea41ad3533c96851d
nikhilagr <nikhil.agrawal005@gmail.com>
2019-02-06 19:08:48 -0800

    Restructuring

--- BullsEye/AboutAuthorViewController.swift
+++ BullsEye/AboutAuthorViewController.swift
@@ -7,24 +7,24 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutAuthorViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var closeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let urlStr = URL(string: "https://nikhildagrawal.com")
+        let request = URLRequest(url: urlStr!)
+        webView.load(request)
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
